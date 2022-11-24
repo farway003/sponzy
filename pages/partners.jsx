44e395a8be40d3ac023b96134a80998db20b965b
farway003 <109739312+farway003@users.noreply.github.com>
2022-11-25 01:59:26 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,7 +16,7 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/6XGqdgE">
+            <a href="https://discord.gg/3cUvbhmrEA">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://i.ibb.co/Qjq2Swy/icons8-source-code-96.png" />
                             <div>
