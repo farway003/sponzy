f9029281c0e1213932e2ebe36d7a5c7ce077b7be
farway003 <109739312+farway003@users.noreply.github.com>
2022-11-24 23:52:58 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -13,8 +13,8 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
 
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
-                        <img width="32" className="rounded-full icon-glow" src="/img/logo.jpg" />
-                        <p className=" font-extrabold ml-1 text-2xl">Astra Bot</p>
+                        <img width="32" className="rounded-full icon-glow" src="/img/logo.png" />
+                        <p className=" font-extrabold ml-1 text-2xl">Sponzy Pro</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
                 </div>
@@ -83,7 +83,7 @@ const Header = ({ $, NavItems }) => {
                                 width="48" height="48"
                             />
                             <p className="invisible md:visible text-xl text-white font-semibold">
-                              <a href="/"><span className="text-amber-400">Astra</span>Bot</a>
+                              <a href="/"><span className="text-amber-400">Sponzy</span>Pro</a>
                             </p>
                         </div>
                         <ul className="hidden lg:flex items-center space-x-4">
@@ -168,7 +168,7 @@ const Header = ({ $, NavItems }) => {
                         </Menu>
                         <Link href="https://bit.ly/3PHDjyC">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
-                               Invite
+                               Davet Et
                             </a>
                </Link>
                     </div>
